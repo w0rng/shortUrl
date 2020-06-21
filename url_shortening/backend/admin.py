861e03f07a5bfd03c274af747e21d220c3cc7292
w0rng <antonabramov2000@gmail.com>
2020-06-21 15:16:54 +0700

    Поля модели в админке теперь отображаются адекватно

--- url_shortening/backend/admin.py
+++ url_shortening/backend/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 import backend.models as models
 
-admin.site.register(models.Link)
+
+@admin.register(models.Link)
+class TimetableGroupAdmin(admin.ModelAdmin):
+    list_display = ('long_link', 'short_link', 'number_clicks')
