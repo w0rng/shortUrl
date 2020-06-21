6404ed2a14638534154f85264d52dcafd855dc37
w0rng <antonabramov2000@gmail.com>
2020-06-21 15:16:28 +0700

    URL главной страницы и адреса для сокращения

--- url_shortening/backend/urls.py
+++ url_shortening/backend/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 import backend.views as views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('<slug:short_link>', views.redirect_original),
+    path('makeshort/', views.shorten_url)
 ]
