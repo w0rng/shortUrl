8322320cd6709aa750b8dc786278922a855ea59d
w0rng <antonabramov2000@gmail.com>
2020-06-21 15:17:19 +0700

    Главная страница и ссылка для сокращения url

--- url_shortening/backend/views.py
+++ url_shortening/backend/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
+import json
 import backend.models as models
 
 
@@ -7,3 +8,18 @@ def redirect_original(request, short_link):
     url = get_object_or_404(models.Link, short_link=short_link)
     url.clicked()
     return HttpResponseRedirect(url.long_link)
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def shorten_url(request):
+    url = request.POST.get("url", '')
+    if url:
+        b = models.Link(long_link=url)
+        b.save()
+
+        response_data = {'url': f'http://127.0.0.1:8000/{b.short_link}'}
+        return HttpResponse(json.dumps(response_data),  content_type="application/json")
+    return HttpResponse(json.dumps({"error": "error occurs"}), content_type="application/json")
